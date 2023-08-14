8d1b1ccd463ce786103b2c54bffde1efa779a48e
aliriodi <aliriodi@gmail.com>
2023-08-13 22:20:54 -0300

    nodemail andando a traves de api en /api/mail  se pasa por body raw json  {to:email, text:'texto', subject:'asunto'}

---
+++ pages/api/mail/index.jsx
@@ -0,0 +1,25 @@
+import {transporter} from '../../../lib/sendEmail';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const { to, subject, text } = req.body;
+// console.log(req.body)
+  try {
+    const mailOptions = {
+      from: 'espanolconeacademy@gmail.com', // Cambia esto a tu dirección de correo electrónico
+      to:  to,
+      subject: subject,
+      text: text
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+    return res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return res.status(500).json({ message: 'Error sending email' });
+  }
+}
