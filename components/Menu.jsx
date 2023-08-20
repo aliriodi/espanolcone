09cae48bc370b77b49544070de30f29cca31b0a7
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-08-20 15:58:49 -0600

    adjust

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -82,67 +82,83 @@
 //     )
 // }
 
-import React, { useState } from 'react';
-import Image from 'next/image'
-import Logo from '../public/imgs/logo-primary.png'
-import Link from 'next/link'
+import React, { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import Logo from '../public/imgs/logo-primary.png';
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+
+        // Agregar el event listener cuando se monta el componente
+        document.addEventListener('click', handleClickOutside);
+
+        // Limpiar el event listener cuando se desmonta el componente
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <>
-            <div className="bg-white flex flex-col fixed px-1 py-1 h-screen">
-                {/* Opciones */}
-                <nav className="my-8" style={{ fontWeight: '500' }}>
-                    {/* Botón para abrir/cerrar el menú */}
-                    <button
-                        className="p-2 rounded-md border border-gray-clear flex item-center my-5"
-                        onClick={() => setIsOpen(!isOpen)}
-                    >
-                        <FontAwesomeIcon icon={faAngleRight} />
-                    </button>
+        <div className="bg-white flex flex-col fixed px-1 py-1 h-screen" ref={menuRef}>
+            {/* Opciones */}
+            <nav className="my-8" style={{ fontWeight: '500' }}>
 
-                    {/* Contenido del menú */}
-                    {isOpen && (
-                        <div>
-                            {/* Logo */}
-                            <Link href="/home">
-                                <Image src={Logo} className='' style={{ width: '108px' }} alt="logo" />
-                            </Link>
-                            <ul className="ml-auto">
-                                <li>
-                                    <Link href="/home">
-                                        <div className="flex item-center mr-20 self-center">
-                                            <FontAwesomeIcon icon={faHouse} className="mr-2" />
-                                            <p>Inicio</p>
-                                        </div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/home">
-                                        <div className="flex item-center mr-20 self-center">
-                                            <FontAwesomeIcon icon={faHouse} className="mr-2" />
-                                            <p>Clases</p>
-                                        </div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/home">
-                                        <div className="flex item-center mr-20 self-center">
-                                            <FontAwesomeIcon icon={faHouse} className="mr-2" />
-                                            <p>Logros</p>
-                                        </div>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    )}
-                </nav>
-            </div>
-        </>
+
+                {/* Contenido del menú */}
+                {isOpen && (
+                    <div>
+                        {/* Logo */}
+                        <Link href="/home">
+                            <Image src={Logo} className='mb-5 ml-8 mt-5' style={{ width: '108px' }} alt="logo" />
+                        </Link>
+                        <ul className="ml-auto ">
+                            <li>
+                                <Link href="/home">
+                                    <div className="flex item-center mb-5 mr-20 self-center">
+                                        <FontAwesomeIcon icon={faHouse} className="mr-2" />
+                                        <p>Inicio</p>
+                                    </div>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/home">
+                                    <div className="flex item-center mb-5 mr-20 self-center">
+                                        <FontAwesomeIcon icon={faHouse} className="mr-2" />
+                                        <p>Clases</p>
+                                    </div>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/home">
+                                    <div className="flex item-center mr-20 self-center">
+                                        <FontAwesomeIcon icon={faHouse} className="mr-2" />
+                                        <p>Logros</p>
+                                    </div>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+                {/* Botón para abrir/cerrar el menú */}
+                <button
+                    className="p-2 rounded-md border border-gray-clear flex item-center my-5"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <FontAwesomeIcon icon={faAngleRight} />
+                </button>
+            </nav>
+        </div>
     );
 };
 
