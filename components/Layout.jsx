c544e8e1f70c64ad83e2d98f079ea7786a026c8a
Ian Franco <nahuelescujuri@gmail.com>
2023-08-27 15:05:55 -0300

    Sign In base funcionando

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children, className = "" }) => {
   return (
-    <div className={`w-full h-full bg-light dark:bg-white p-4 sm:px-4 md:px-8 lg:px-8 xl:px-8 2xl:px-8 ${className}`}>
+    <div className={`w-full h-full ${className}`}>
       <div className="mx-auto">{children}</div>
     </div>
   );
