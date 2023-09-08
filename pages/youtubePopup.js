c1b41adc23537e57b660184265b346384de899d5
aliriodi <aliriodi@gmail.com>
2023-09-07 22:51:57 -0300

    intentando de renderizar puntuacion en puntos

--- pages/youtubePopup.js
+++ pages/youtubePopup.js
@@ -6,7 +6,7 @@ export default function YoutubePopup() {
 
     return (
       <div className='mx-auto my-auto h-1/2 relative' style={{width:"640px", marginTop:"100px"}}>
-         En el siguiente video, selecciona la forma correcta de los verbos para completar las oraciones.
+         
          {/* <Youtubet videoId={'UeVy4QOUBy8'} 
                    popups={[
                     // Objetos de Pruebas
@@ -17,21 +17,23 @@ export default function YoutubePopup() {
                                            type:"checkbox",options:['Sacar','bolea','derecha'], paddingLeft:'38%',popUpShown: false},
                   ]} /> */}
                   <Youtubet videoId={'CguXO_5okCY'} 
-                   popups={[
+                            titlep={'En el siguiente video, selecciona la forma correcta de los verbos para completar las oraciones.'}
+                   popups={
+                      [
                     // Objetos de Pruebas
-                    {time:9, title:"Mi nombre _______ Daniela Masías.", reply:"es", message:" (soy/es/está) ", 
+                    {time:11, title:"Mi nombre _______ Daniela Masías.", reply:"es", message:" (soy/es/está) ", 
                                            type:"checkbox",options:['soy','es','está'],paddingLeft:'10%',popUpShown: false,value:0},
-                    {time:54, title:"Ella es Julia Coronatta y _______ 22 años.", message:"Pienselo bien", reply:"tiene", 
+                    {time:56, title:"Ella es Julia Coronatta y _______ 22 años.", message:"Pienselo bien", reply:"tiene", 
                                            type:"checkbox",options:['tiene','lleva','es'],paddingLeft:'10%',popUpShown: false,value:0},
-                    {time:81, title:"Ella ______ de Alemania.", message:"EL deporte es", reply:"es" , 
+                    {time:83, title:"Ella ______ de Alemania.", message:"EL deporte es", reply:"es" , 
                                            type:"checkbox",options:['tiene','es','esta'],paddingLeft:'10%',popUpShown: false,value:0},
-                    {time:114, title:"Yo _______ de Minneapolis Minesota.", message:"Actividad 1", reply:"vengo", 
+                    {time:116, title:"Yo _______ de Minneapolis Minesota.", message:"Actividad 1", reply:"vengo", 
                                            type:"checkbox",options:['vengo','viene','venir'], paddingLeft:'10%',popUpShown: false,value:0},
-                    {time:150, title:"Susana López viene de México, de una ciudad pequeña que _________ Zamora.", message:"Actividad 1", reply:"se llama", 
+                    {time:152, title:"Susana López viene de México, de una ciudad pequeña que _________ Zamora.", message:"Actividad 1", reply:"se llama", 
                                            type:"checkbox",options:['es','se llama','esta'], paddingLeft:'10%',popUpShown: false,value:0},
-                    {time:191, title:"Me _______ las ciudades pequeñas.", message:"Actividad 1", reply:"gustan", 
+                    {time:193, title:"Me _______ las ciudades pequeñas.", message:"Actividad 1", reply:"gustan", 
                                            type:"checkbox",options:['gusta','gustar','gustan'], paddingLeft:'10%',popUpShown: false,value:0},
-                    {time:216, title:"Córdoba _____ una provincia preciosa.", message:"Actividad 1", reply:"es", 
+                    {time:218, title:"Córdoba _____ una provincia preciosa.", message:"Actividad 1", reply:"es", 
                                            type:"checkbox",options:['está','son','es'], paddingLeft:'10%',popUpShown: false,value:0},
                   ]} />
         
