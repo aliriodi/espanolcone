c023f0cd67f87b6ad180fa0fd5fca31f2dc0e249
aliriodi <aliriodi@gmail.com>
2023-09-11 14:52:57 -0300

    about us Optimizado a su  minima expresion

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,14 +1,11 @@
-import React , {useEffect }from 'react';
-import Image from 'next/image';
+import React from 'react';
 import Head from 'next/head';
 import Navbar from '../components/Navbar/Navbar';
-import { useRouter } from 'next/router';
 import nextI18NextConfig from "../next-i18next.config";
 import { useTranslation , withTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
    function Aboutus() {
-     const { locale, locales, push } = useRouter()
      const { t } = useTranslation('aboutus')
   
   return (
@@ -16,12 +13,13 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
         <Head>
         <title>Español con E | Bienvenidos</title>
         <meta name="landing" content="welcome" />
-      </Head>
-            <Navbar /> 
+        </Head>
+
+        <Navbar /> 
       
         <div>{t('aboutus')}</div>
    
-        <p>aboutus</p>
+             <p>aboutus</p>
        
   </div>
   )
