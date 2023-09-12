6fb8e635c0eeca636fb89fe8b2b17d2b0d7d0999
aliriodi <aliriodi@gmail.com>
2023-09-12 11:53:52 -0300

    googleanalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import ReactGA, { initialize } from "react-ga";
 import { useSession, SessionProvider } from "next-auth/react";
 import { appWithTranslation } from 'next-i18next'
 import nextI18NextConfig from '../next-i18next.config';
 
+const TRAKING_ID ='G-DVGE2GBTSL';
+ReactGA,initialize(TRAKING_ID);
 function App({ Component, pageProps }) {
 
   return (
