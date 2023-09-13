80416d330bbf9e385c1e4c32d080d40b566f72e2
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-09-13 15:13:41 -0600

    cambios en los sliders

--- components/landingComponents/Experts.jsx
+++ components/landingComponents/Experts.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Slider from "react-slick";
+import Image from 'next/image';
 import { experts } from '../../public/imgs/images.js';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Experts() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
+
   return (
-    <>
-      <div>
-        <p className=' flex items-center justify-center text-4xl'>Nuestro Equipo</p>
-      </div>
-      <div className='grid grid-cols-4 gap-4 '>
-        {experts.map((image, index) => (
-          <div key={index} className='col-span-1 my-10 mx-20'>
-            <Image alt={image.name} src={image.image} width={300} height={200} />
-            <div className='flex items-center justify-center flex-col'>
-              <p className='text-xl font-semibold'>{image.namePerson}</p>
-              <p>{image.job}</p>
+    <div>
+      <p className='flex items-center justify-center text-4xl'>Nuestro Equipo</p>
+      <div className='mt-5'>
+        <Slider {...settings}>
+          {experts.map((image, index) => (
+            <div key={index} className='px-2'>
+              <div className='my-10 mx-20'>
+                <Image alt={image.name} src={image.image} width={300} height={200} />
+                <div className='flex items-center justify-center flex-col'>
+                  <p className='text-xl font-semibold'>{image.namePerson}</p>
+                  <p>{image.job}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
-    </>
-  )
+    </div>
+  );
 }
