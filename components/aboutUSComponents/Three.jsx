662e734b4f30133cf5b79285ee83a4f4ea899b0c
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-09-13 05:48:12 -0600

    ajuste imagen buscador

--- components/aboutUSComponents/Three.jsx
+++ components/aboutUSComponents/Three.jsx
@@ -44,7 +44,7 @@ export default function Three() {
             <h2 className="text-xl font-semibold text-white">Compromiso</h2>
           </div>
         </div>
-        <div className="grid grid-cols-3 bg-[#dadbdf]">
+        <div className="grid grid-cols-3 h-[15em] bg-[#dadbdf]">
           <div className=" py-4 mx-[220px] text-center flex items-center ">
             <p>Garantizar el aprendizaje del español de manera atractiva para todos las personas.</p>
           </div>
@@ -53,15 +53,17 @@ export default function Three() {
           </div>
           <div className=" py-4 mx-[220px] text-center flex items-center">
             <p>Difundir la lengua y la cultura de los países hispanohablantes en todo el mundo.</p>
-            <div className=' flex items-center justify-end mb-[-100px] z-20'>
-              <Image
-                src="https://res.cloudinary.com/dfddh08q8/image/upload/v1694366429/images/icon_desc_about_wxyaz9.png"
-                alt="Teléfono"
-                width={150}
-                height={300}
-              />
-            </div>
+
           </div>
+
+        </div>
+        <div className=' flex items-end justify-end mt-[-100px] w-full pr-[200px]'>
+          <Image
+            src="https://res.cloudinary.com/dfddh08q8/image/upload/v1694366429/images/icon_desc_about_wxyaz9.png"
+            alt="buscador"
+            width={150}
+            height={300}
+          />
         </div>
 
       </div>
