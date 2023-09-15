b66ad095b457b0189de2ff61a7f3744e86f38f77
aliriodi <aliriodi@gmail.com>
2023-09-15 06:36:19 -0300

    check

--- pages/prueba.js
+++ pages/prueba.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Class from "../components/Class/Class"
 
 export default function prueba() {
   return (
-    <div>hola Nahuel</div>
+    <div>hola Nahuel
+      <p></p>
+      <Class />
+    </div>
   )
 }
