46eab77d8821ea492f1c15ea3318baaae8db3959
aliriodi <aliriodi@gmail.com>
2023-09-24 12:21:15 -0300

    clase cargada

---
+++ pages/api/class/get.js
@@ -0,0 +1,37 @@
+import dbConnect from '../../../config/mongo'
+import Class from '../../../models/Class'
+
+// /**
+//  * 
+//  * @param {import('next').NextApiRequest} req 
+//  * @param {import('next').NextApiResponse}  res 
+//  */
+
+
+export default async function getAllClass(req, res) {
+
+  try {
+    // const { first_name, last_name, email } = req.body;
+
+    console.log('CONNECTING TO MONGO DB');
+
+    await dbConnect()
+
+    console.log('CONNECTED TO MONGO DB');
+
+
+
+    console.log('CREATING DOCUMENT');
+
+    const class1 = await Class.find().exec();
+
+    console.log('CREATED DOCUMENT');
+
+    res.json({ class1 })
+
+  } catch (error) {
+    console.log(error);
+    res.json({ error })
+  }
+}
+
