7ef77335fc8ea3bcaf493fff45decd189b8fafb5
Alirio Diaz <aliriodi@gmail.com>
2023-09-30 16:27:46 -0300

    Update boxmove.jsx
    pisando boxmove por alkirio

--- components/Class/boxmove.jsx
+++ components/Class/boxmove.jsx
@@ -1,93 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../styles/boxmove.module.css'
-import Image from 'next/image';
-export default function boxmove(props) {
-    function dragstart_handler(ev) {
-        // Add the target element's id to the data transfer object
-        ev.dataTransfer.setData("text/plain", ev.target.id);
-      }
-    
-      window.addEventListener("DOMContentLoaded", () => {
-        // Get the element by id
-        const element = document.getElementById(props.id);
-        // Add the ondragstart event listener
-        element.addEventListener("dragstart", dragstart_handler);
-      });
-    
-   let   onDrop = (ev, cat) => {         
-        let id = ev.dataTransfer.getData(props.id);  
-        let tasks = state.tasks.filter((task) => {      
-            if (task.name == id) {               
-                task.category = cat;                 }                     
-                return task;          
-         });           
-         setState({                 
-             ...state,                 
-            tasks          
-         });    
-   
-    } ; let    onDragOver = (ev)=>{ ev.preventDefault();}
+import { Rnd } from "react-rnd";
+//https://geekflare.com/es/best-drag-and-drop-react-libraries/
+export default function Boxmove(props) {
+    //levo el array de objetos con [{value,id}] a un array de valores b = [option1,option2,...]
+    // const b  = props.options.map(option=>option.value) 
+    const [state, setState] = useState({ x: props.option.x, y: props.option.y })
+    const [boxShadowcolor,setBoxShadowcolor]=useState('0px 0px 10px yellow')
 
-//forma div
-//Make the DIV element draggagle:
-dragElement(document.getElementById("mydiv"));
-alert(document.getElementById("mydiv"))
-console.log(document.getElementById("mydiv"))
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+    return (
+        <div >
+            {<div key={props.option.id} className={styles[props.option.id]}>
+                <Rnd
+                    size={{ width: props.option.width, height: props.option.height }}
+                    //bounds={{ top: yMin, right: xMax, bottom: yMax, left: xMin }}
+                    //bounds={{ top: 653, right: 1024 , bottom: 726, left: 723 }}
+                    position={{ x: state.x, y: state.y }}
+                    onDragStop={(e, d) => {if(180<d.x&&d.x<600&&-158<d.y&&d.y<-12){setState({x:d.x,y:d.y})}else{setState({ x: props.option.x, y: props.option.y })}
+                                        console.log('x:',d.x,'y:',d.y,'option:',props.option.value)
+                                         if(props.option.xmin<d.x&&d.x<props.option.xmax&&props.option.ymin<d.y&&d.y<props.option.ymax){setBoxShadowcolor('0px 0px 10px green')}
+                                         else(setBoxShadowcolor('0px 0px 10px red'))  
+                                         //setState({ x: d.x, y: d.y });
+                                         //console.log(d) 
+                                 }}
+                                     //if(319<d.x<497){setState({x:d.x,y:state.y}else{setState({ x: props.option.x, y: props.option.y })})}
+                                      //if(723<d.x<1024 && 653<d.y<726){setState({ x: d.x, y: d.y }) }else(setState({ x: props.option.x, y: props.option.y }))
+                    onDrag={(e, d) => { 
+                                  // Este evento se dispara continuamente mientras se arrastra
+                       // console.log('Nueva posición durante el arrastre:', { x: d.x, y: d.y , valor:props.option.value});
+                        
+                        //   setState({ x: d.x, y: d.y });
+                      }}
+                    
+                      
+                    style={{
+                        zIndex: 1000,
+                        backgroundColor: '#4CCFEB',
+                        display:'flex',
+                        alignItems: 'center', // Centra verticalmente
+                        justifyContent: 'center', // Centra horizontalmente
+                        borderRadius: '8px', // Agrega el radio de borde que desees
+                        boxShadow:boxShadowcolor,
+                    }}
+                >{props.option.value}</Rnd></div>
+            }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+        </div>
+    )
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
-  return (
-    <div>
-  <div><label draggable={false}>{props.id}</label></div>
-  <div  hey={props.id} onDragStart={(e)=>onDragStart(e,props.id)}
-       draggable className="droppable" ><label id={props.id} draggable className="droppable" onDragOver={(e)=>onDragOver(e)}  
-              onDrop={(e)=>onDrop(e, "complete")}>{props.option}</label></div>
-
-              
-<div id="mydiv">
-  <div id="mydivheader">Click here to move</div>
-  <p>Move</p>
-  <p>this</p>
-  <p>DIV</p>
-</div>
-    </div>
-  )
 }
