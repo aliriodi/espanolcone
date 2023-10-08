6f1cac3c0c35f89e94797191b394a2e0a2410b98
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-10-07 19:02:12 -0600

    cambios en el date picker

--- pages/teachers.js
+++ pages/teachers.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Menu from '../components/Menu'
 import Image from 'next/image'
 import TeachersCard from '../components/TeachersCard'
+import DatePi from './../components/DatePicker';
 
 export default function teachers() {
   return (
     <>
       <Menu />
       <div className='ml-[80px] w-full flex flex-wrap'>
-
+        <DatePi />
         <TeachersCard />
 
       </div>
