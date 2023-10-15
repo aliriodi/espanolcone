ecfaf25ccd705dcd2a5e8e7c870f106e985dfc9d
aliriodi <aliriodi@gmail.com>
2023-10-15 13:11:47 -0300

    carga de algunos estilos

---
+++ pages/inicio/preview.js
@@ -0,0 +1,28 @@
+import React ,{useEffect} from 'react';
+import { useSelector     } from 'react-redux'
+import Menu from "../../components/Menu";
+import PREVIEW from "../../components/ClassLoad/Classpreview";
+import style from "../../styles/courses.module.css"
+export default function preview() {
+    const classpreview = useSelector((state) => state.datos.classpreview);
+    useEffect(() => {
+        {console.log('otroUSEEFFECT')}
+        {console.log(classpreview)}
+    }, [classpreview]);
+  return (
+    <div className={style['container']}>
+      <div className={style['container0']}>
+        <Menu /></div>
+      <div className='ml-[80px] '>
+        {classpreview ? 
+          <div className={style['container1']}>
+          <PREVIEW class={classpreview}></PREVIEW></div> 
+          :
+          <div classname='flex items-center justify-center h-screen'>
+            <Spinner className="mx-auto my-auto" />
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
