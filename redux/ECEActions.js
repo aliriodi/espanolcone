0f4c6c99c678de16a1e1c2aa4e6742e7bf0c95ef
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-10-16 17:14:11 -0600

    arregalndo el slice y las actions

--- redux/ECEActions.js
+++ redux/ECEActions.js
@@ -1,39 +1,41 @@
-    getUserS,
-    showClass,
-    classId,
-    classPreviewS
-  } from "./ECESlice";
-  
-  export const getuser = () => async (dispatch) => {
-    await fetch("/api/hello")
-      .then((response) => response.json())
-      .then((json) => dispatch(getUserS(json)))
-      .catch((error) => console.log(error));
-  };
+import {
+  getUserS,
+  showClass,
+  classId,
+  classPreviewS,
+  setCards,
+} from "./ECESlice";
 
-  export const setshowClass = (show) => (dispatch)=>   {
-    
-   // console.log(show)
-     dispatch(showClass(show))
-  };
-  export const classid = (id) => (dispatch)=>   {
-    // console.log('entre a redux')
-    // console.log(id)
-      dispatch(classId(id))
-   };
-  
-  export const getuseremail = (email,password) => async (dispatch) => {
-    await fetch("/api/users/getid?email="+email+"&&password="+password)
-      .then((response) => response.json())
-      .then((json) => dispatch(getUserS(json)))
-      .catch((error) => console.log(error));
-  };
+export const getuser = () => async (dispatch) => {
+  await fetch("/api/hello")
+    .then((response) => response.json())
+    .then((json) => dispatch(getUserS(json)))
+    .catch((error) => console.log(error));
+};
 
-  export const classpreviewA = (classp) => (dispatch) =>   {
-     console.log('entre a redux classpreview')
-    // console.log(id)
-      dispatch(classPreviewS(classp))
-   };
+export const setshowClass = (show) => (dispatch) => {
+
+  // console.log(show)
+  dispatch(showClass(show))
+};
+export const classid = (id) => (dispatch) => {
+  // console.log('entre a redux')
+  // console.log(id)
+  dispatch(classId(id))
+};
+
+export const getuseremail = (email, password) => async (dispatch) => {
+  await fetch("/api/users/getid?email=" + email + "&&password=" + password)
+    .then((response) => response.json())
+    .then((json) => dispatch(getUserS(json)))
+    .catch((error) => console.log(error));
+};
+
+export const classpreviewA = (classp) => (dispatch) => {
+  console.log('entre a redux classpreview')
+  // console.log(id)
+  dispatch(classPreviewS(classp))
+};
 
 export const fetchTeachers = () => async (dispatch) => {
   try {
@@ -50,5 +52,5 @@ export const fetchTeachers = () => async (dispatch) => {
     console.error('Error al cargar los datos de profesores:', error);
   }
 };
-  
+
 
