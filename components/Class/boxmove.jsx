ba0aa7c9b59fad4faf5ebfff27b4ca817b99759d
aliriodi <aliriodi@gmail.com>
2023-10-18 22:56:49 -0300

    cargando hasta la pag 9 nivel b1 unidad 1

--- components/Class/boxmove.jsx
+++ components/Class/boxmove.jsx
@@ -1,110 +1,257 @@
-import React, { useState } from 'react';
+// import React, { useState , useEffect} from 'react';
+// import styles from '../../styles/boxmove.module.css';
+// import { Rnd } from "react-rnd";
+
+// export default function Boxmove(props) {
+//   const [state, setState] = useState({ x: props.option.x, y: props.option.y });
+//   const [boxShadowcolor, setBoxShadowcolor] = useState('0px 0px 20px yellow');
+//   const [cXmax, setCXmaxe] = useState(0); 
+//   // Las coordenadas del lugar correcto donde quieres incrustar la caja
+//   const correctPosition = { x: (0)/2, y: (0)/2 };
+//  //Seleccionando el donumento <div id="value"> que tiene id=valor de la solucion real
+//   const sol = document.querySelector("#"+props.option.value)
+// //Seleccionando el documento <span id='valor1'> que tiene id numero de valor
+//   const sol1 = document.querySelector("#"+props.option.id)
+//  //si los componentes con los div id o span id existen hacer 
+//  useEffect(() => {
+
+//   if (sol && sol1) {
+  
+//     // Obtenemos las coordenadas y dimensiones del elemento "sol"
+//     sol1.addEventListener("mouseup", function (event) { 
+    
+//       // Obtenemos las coordenadas del elemento "sol1" relativas a su elemento padre (en este caso, "sol")
+//       const sol1Rect = sol1.getBoundingClientRect();
+//       const solRect = sol.getBoundingClientRect();
+  
+
+//     if(cXmax<sol1Rect.left){
+      
+//       setCXmaxe(sol1Rect.right)
+//     }
+//   //  console.log(typeof(sol1Rect.left),solRect.left)
+//     const correctPosition = { x: (0.5*sol1Rect.left+0.5*sol1Rect.right), y: (0.5*sol1Rect.top+0.5*sol1Rect.bottom) };
+//       // Comparamos las coordenadas para verificar si "sol1" está contenido dentro de "sol"
+//     if (event.button === 0 &&
+//       sol1Rect.top +10>= solRect.top &&
+//       sol1Rect.bottom-10 <= solRect.bottom &&
+//       sol1Rect.left >= solRect.left &&
+//       sol1Rect.right <= solRect.right
+//     ) {
+    
+//   //  console.log('correct position',correctPosition)
+//   //  console.log((solRect.top+solRect.bottom)/2)
+//   //  console.log((sol1Rect.top+sol1Rect.bottom)/2)
+    
+     
+//       setBoxShadowcolor('0px 0px 20px green');
+      
+//     } else {
+//       //console.log("sol1 no está dentro de sol.");
+//       //console.log(solRect.top)
+//       //console.log(sol1Rect.top) 
+//       setBoxShadowcolor('0px 0px 20px red');
+//       if (event.button === 0 && (
+//         Math.abs(sol1Rect.top -solRect.top) >= 200 ||
+//         sol1Rect.bottom-solRect.bottom <=-100  ||
+//         sol1Rect.left -solRect.left >= 310 ||
+//         sol1Rect.right-solRect.right <= -100)
+//       ) {
+//        // console.log('arriba',sol1Rect.top -solRect.top)
+//        // console.log('abajo',sol1Rect.bottom-solRect.bottom)
+//        // console.log('izquierda',sol1Rect.left -solRect.left)
+//       //  console.log('derecha',sol1Rect.right-solRect.right)
+//      setState({x: props.option.x, y: props.option.y})
+//       }
+//     };
+   
+//   });
+// }
+
+  
+
+//   // Eliminar el event listener cuando el componente se desmonta
+//   return () => {
+//     if (sol&&sol1) {
+//       sol1.removeEventListener("mouseup", function (event) { 
+    
+//         // Obtenemos las coordenadas del elemento "sol1" relativas a su elemento padre (en este caso, "sol")
+//         const sol1Rect = sol1.getBoundingClientRect();
+//     const solRect = sol.getBoundingClientRect();
+     
+//       if(cXmax<sol1Rect.left){setCXmaxe(sol1Rect.right)}
+//           const correctPosition = { x: (0.5*sol1Rect.left+0.5*sol1Rect.right), y: (0.5*sol1Rect.top+0.5*sol1Rect.bottom) };
+//           if (event.button === 0 &&
+//         sol1Rect.top +10>= solRect.top &&
+//         sol1Rect.bottom-10 <= solRect.bottom &&
+//         sol1Rect.left >= solRect.left &&
+//         sol1Rect.right <= solRect.right
+//       ) {
+//               setBoxShadowcolor('0px 0px 20px green');
+        
+//       } else {
+//               setBoxShadowcolor('0px 0px 20px red');
+//         if (event.button === 0 && (
+//           Math.abs(sol1Rect.top -solRect.top) >= 200 ||
+//           sol1Rect.bottom-solRect.bottom <=-100  ||
+//           sol1Rect.left -solRect.left >= 310 ||
+//           sol1Rect.right-solRect.right <= -100)
+//         ) {
+//             setState({x: props.option.x, y: props.option.y})
+//         }
+//       };
+     
+//     });
+//     }
+//   };
+// }, []); 
+
+  
+//     // if(sol&&sol1){ 
+   
+//   // sol.addEventListener("mouseup", function() {
+//   //  //console.log(sol1.id.substring(0, sol1.id.length - 1))
+//   //  // Esta función se ejecutará cuando se haga clic en el elemento con id="sol"
+//   //   if(sol.id===sol1.id.substring(0, sol1.id.length - 1)){
+//   //     //console.log(sol1)
+//   //     console.log(sol.id)
+//   //     console.log(sol1.id)
+//   //   console.log("Se hizo clic en el elemento "+props.option.value);}
+//   // });
+// // }
+
+//   const handleDrag = (e, d) => {
+//     // Calcula la distancia entre la caja y el lugar correcto
+//     const distance = Math.sqrt((d.x - correctPosition.x) ** 2 + (d.y - correctPosition.y) ** 2);
+
+//     // Establece un umbral de distancia para determinar cuándo se debe incrustar la caja
+//     const threshold = 40;
+//    // console.log('distance',distance)
+//     // if(180<d.x&&d.x<600&&-158<d.y&&d.y<-12){setState({x:d.x,y:d.y})}else{setState({ x: props.option.x, y: props.option.y })}
+//     setState({x:d.x,y:d.y})
+//     if (distance < threshold) {
+//       // Si la caja está lo suficientemente cerca, ajusta la posición al lugar correcto
+//       setState(correctPosition);
+    
+//       setBoxShadowcolor('0px 0px 20px green');
+//     } else {
+//       // Si la caja se aleja del lugar correcto, mantén la posición actual
+//       setState({ x: d.x, y: d.y });
+//       setBoxShadowcolor('0px 0px 20px red');
+    
+//     }
+//   };
+ 
+
+//   return (
+//     <div>
+//       <div key={props.option.id} className={styles[props.option.id]}>
+//         <Rnd
+//         key={props.option.id}
+//           id={props.option.id}
+//           size={{ width: props.option.width, height: props.option.height }}
+//           position={state}
+//           onDragStop={handleDrag}
+//           style={{
+//             zIndex: 1000,
+//             backgroundColor: '#4CCFEB',
+//             display: 'flex',
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//             borderRadius: '8px',
+//             boxShadow: boxShadowcolor,
+//           }}
+//         >
+//           {/* {props.option.value}+ */}
+//           {props.option.id}
+//         </Rnd>
+//       </div>
+//     </div>
+//   );
+// }
+
+import React, { useState, useEffect } from 'react';
 import styles from '../../styles/boxmove.module.css';
 import { Rnd } from "react-rnd";
 
 export default function Boxmove(props) {
   const [state, setState] = useState({ x: props.option.x, y: props.option.y });
   const [boxShadowcolor, setBoxShadowcolor] = useState('0px 0px 20px yellow');
-  const [cXmax, setCXmaxe] = useState(0); 
-  // Las coordenadas del lugar correcto donde quieres incrustar la caja
-  const correctPosition = { x: (0)/2, y: (0)/2 };
- //Seleccionando el donumento <div id="value"> que tiene id=valor de la solucion real
-  const sol = document.querySelector("#"+props.option.value)
-//Seleccionando el documento <span id='valor1'> que tiene id numero de valor
-  const sol1 = document.querySelector("#"+props.option.id)
- //si los componentes con los div id o span id existen hacer 
+  const [cXmax, setCXmax] = useState(0);
 
+  const correctPosition = { x: 0, y: 0 };
 
- if (sol && sol1) {
-  
-    // Obtenemos las coordenadas y dimensiones del elemento "sol"
-    sol1.addEventListener("mouseup", function (event) { 
-    
-      // Obtenemos las coordenadas del elemento "sol1" relativas a su elemento padre (en este caso, "sol")
+  // Función que maneja el evento de mouseup
+  function handleMouseUp  (event) {
+    const sol = document.querySelector("#" + props.option.value);
+    const sol1 = document.querySelector("#" + props.option.id);
+
+    if (sol && sol1) {
       const sol1Rect = sol1.getBoundingClientRect();
-  const solRect = sol.getBoundingClientRect();
-  
+      const solRect = sol.getBoundingClientRect();
 
-    if(cXmax<sol1Rect.left){setCXmaxe(sol1Rect.right)}
-  //  console.log(typeof(sol1Rect.left),solRect.left)
-    const correctPosition = { x: (0.5*sol1Rect.left+0.5*sol1Rect.right), y: (0.5*sol1Rect.top+0.5*sol1Rect.bottom) };
-      // Comparamos las coordenadas para verificar si "sol1" está contenido dentro de "sol"
-    if (event.button === 0 &&
-      sol1Rect.top +10>= solRect.top &&
-      sol1Rect.bottom-10 <= solRect.bottom &&
-      sol1Rect.left >= solRect.left &&
-      sol1Rect.right <= solRect.right
-    ) {
-    
-  //  console.log('correct position',correctPosition)
-  //  console.log((solRect.top+solRect.bottom)/2)
-  //  console.log((sol1Rect.top+sol1Rect.bottom)/2)
-    
-     
-      setBoxShadowcolor('0px 0px 20px green');
-    } else {
-      //console.log("sol1 no está dentro de sol.");
-      //console.log(solRect.top)
-      //console.log(sol1Rect.top) 
-      setBoxShadowcolor('0px 0px 20px red');
-      if (event.button === 0 && (
-        Math.abs(sol1Rect.top -solRect.top) >= 200 ||
-        sol1Rect.bottom-solRect.bottom <=-100  ||
-        sol1Rect.left -solRect.left >= 310 ||
-        sol1Rect.right-solRect.right <= -100)
-      ) {
-       // console.log('arriba',sol1Rect.top -solRect.top)
-       // console.log('abajo',sol1Rect.bottom-solRect.bottom)
-       // console.log('izquierda',sol1Rect.left -solRect.left)
-      //  console.log('derecha',sol1Rect.right-solRect.right)
-     setState({x: props.option.x, y: props.option.y})
+      if (cXmax < sol1Rect.left) {
+        setCXmax(sol1Rect.right);
       }
-    };
-   
-  });
-}
 
-  
-    // if(sol&&sol1){ 
-   
-  // sol.addEventListener("mouseup", function() {
-  //  //console.log(sol1.id.substring(0, sol1.id.length - 1))
-  //  // Esta función se ejecutará cuando se haga clic en el elemento con id="sol"
-  //   if(sol.id===sol1.id.substring(0, sol1.id.length - 1)){
-  //     //console.log(sol1)
-  //     console.log(sol.id)
-  //     console.log(sol1.id)
-  //   console.log("Se hizo clic en el elemento "+props.option.value);}
-  // });
-// }
+      const correctPosition = {
+        x: (0.5 * sol1Rect.left + 0.5 * sol1Rect.right),
+        y: (0.5 * sol1Rect.top + 0.5 * sol1Rect.bottom),
+      };
 
-  const handleDrag = (e, d) => {
-    // Calcula la distancia entre la caja y el lugar correcto
-    const distance = Math.sqrt((d.x - correctPosition.x) ** 2 + (d.y - correctPosition.y) ** 2);
-
-    // Establece un umbral de distancia para determinar cuándo se debe incrustar la caja
-    const threshold = 40;
-   // console.log('distance',distance)
-    // if(180<d.x&&d.x<600&&-158<d.y&&d.y<-12){setState({x:d.x,y:d.y})}else{setState({ x: props.option.x, y: props.option.y })}
-    setState({x:d.x,y:d.y})
-    if (distance < threshold) {
-      // Si la caja está lo suficientemente cerca, ajusta la posición al lugar correcto
-      setState(correctPosition);
-    
-      setBoxShadowcolor('0px 0px 20px green');
-    } else {
-      // Si la caja se aleja del lugar correcto, mantén la posición actual
-      setState({ x: d.x, y: d.y });
-      setBoxShadowcolor('0px 0px 20px red');
-    
+      if (
+        event.button === 0 &&
+        sol1Rect.top + 10 >= solRect.top &&
+        sol1Rect.bottom - 10 <= solRect.bottom &&
+        sol1Rect.left >= solRect.left &&
+        sol1Rect.right <= solRect.right
+      ) {
+        setBoxShadowcolor('0px 0px 20px green');
+      } else {
+        setBoxShadowcolor('0px 0px 20px red');
+        if (
+          event.button === 0 &&
+          (Math.abs(sol1Rect.top - solRect.top) >= 200 ||
+            sol1Rect.bottom - solRect.bottom <= -100 ||
+            sol1Rect.left - solRect.left >= 310 ||
+            sol1Rect.right - solRect.right <= -100)
+        ) {
+          setState({ x: props.option.x, y: props.option.y });
+        }
+      }
     }
   };
- 
+
+  // Agregar el event listener cuando el componente se monta
+  useEffect(() => {
+    window.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      // Quitar el event listener cuando el componente se desmonta
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
+  const handleDrag = (e, d) => {
+    const distance = Math.sqrt((d.x - correctPosition.x) ** 2 + (d.y - correctPosition.y) ** 2);
+    const threshold = 40;
+    setState({ x: d.x, y: d.y });
+
+    if (distance < threshold) {
+      setState(correctPosition);
+      setBoxShadowcolor('0px 0px 20px green');
+    } else {
+      setState({ x: d.x, y: d.y });
+      setBoxShadowcolor('0px 0px 20px red');
+    }
+  };
 
   return (
     <div>
+      
       <div key={props.option.id} className={styles[props.option.id]}>
         <Rnd
+          key={props.option.id}
           id={props.option.id}
           size={{ width: props.option.width, height: props.option.height }}
           position={state}
@@ -125,4 +272,3 @@ export default function Boxmove(props) {
     </div>
   );
 }
-
