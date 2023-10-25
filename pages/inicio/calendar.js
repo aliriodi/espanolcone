65d3cfbd3ae14863585804a8031873867873d150
aliriodi <aliriodi@gmail.com>
2023-10-25 08:45:35 -0300

    calendar user mejorando aun FALTA

--- pages/inicio/calendar.js
+++ pages/inicio/calendar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Calendar from '../../components/Calendar/Calendar'
+import Menu from '../../components/Menu'
 
 export default function Calendar2
 
   () {
   return (
     <>
+      <Menu />
       <Calendar />
     </>
   )
--- pages/inicio/schedule.js
+++ pages/inicio/schedule.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Schedule from '../../components/Calendar/Calendar2'
+import Menu from '../../components/Menu'
 
 export default function Calendar2
 
   () {
   return (
     <>
+      <Menu />
       <Schedule />
     </>
   )
