1809b5c32d88ce1ce4276ca7e96761c2203bea5a
aliriodi <aliriodi@gmail.com>
2023-10-29 13:28:01 -0300

    cargando imagenes apropiadamente en la agenda

--- components/Calendar/Calendar.jsx
+++ components/Calendar/Calendar.jsx
@@ -236,15 +236,15 @@ function Meeting({ meeting }) {
   return (
     <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
       <Image
-        src={meeting.image}
-        alt=""
+        src={meeting?.image?.url || meeting?.image}
+        alt="img"
         className="flex-none w-10 h-10 rounded-full"
         width={160}
         height={160}
-      />{console.log(meeting)}
+      />{console.log(meeting.image)}
       <div className="flex-auto">
-        {meeting.assigned ? <p className="text-gray-900">{meeting.name}</p> : <p className="text-gray-900">Meeting no asignado aún</p>}
-        <p className="text-gray-900">{meeting.nameuser}</p>
+        {meeting.assigned ? true : <p className="text-gray-900">Meeting no asignado aún</p>}
+        <p className="text-gray-900">{meeting.first_name+' '+meeting.last_name}</p>
         <p className="text-gray-900">{meeting.role}</p>
         <p className="mt-0.5">
           <time dateTime={meeting.startDatetime}>
