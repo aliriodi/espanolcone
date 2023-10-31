c1afa7ab40fbe6d3979c723f6186e600ab1d0cf9
aliriodi <aliriodi@gmail.com>
2023-10-31 08:50:54 -0300

    cloudianry andando

--- components/cloudinary/cloudinary.jsx
+++ components/cloudinary/cloudinary.jsx
@@ -1,70 +1,54 @@
 import React, { useState } from "react";
-import { CldUploadButton } from 'next-cloudinary';
 import Image from 'next/image';
 
-
-
-
-
 export default function CloudinaryUploader(props) {
-  const [info, updateInfo] = useState();
-  const [imageUrl, setimageUrl] = useState();
+  const [imageUrl, setImageUrl] = useState(null);
   const [loading, setLoading] = useState(false);
-  
+  const [error, setError] = useState(null);
 
-  const handleUpload = async (e, error, result, widget) => {
+  const handleUpload = async (e) => {
     const file = e.target.files[0];
     setLoading(true);
-    if (!error) {
+
+    try {
       // Create a FormData object and append data to it
       const formData = new FormData();
       formData.append('file', file);
       formData.append("upload_preset", process.env.NEXT_PUBLIC_UPLOAD_PRESET);
-           
-       //console.log(formData)
-      // Send the FormData to your server or perform other actions
-      // For example, you can use the fetch API to upload the FormData
+
+      // Send the FormData to Cloudinary
       const response = await fetch("https://api.cloudinary.com/v1_1/" + process.env.NEXT_PUBLIC_CLOUDINARY_NAME + "/image/upload", {
         method: 'POST',
         body: formData,
-     //   mode: 'cors',
-      //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      //  credentials: 'same-origin', // include, *same-origin, omit
-      }).catch((error) => {
-          console.error('Error uploading image:', error);
-          setLoading(false)
-        });
-        const data = await response.json().then(response=>setimageUrl(response.secure_url));
-        //funcion para escribir en Base ded atos viene por props cuando se llame el componente
-       // props.functionBD(data.secure_url);
-     
-        console.log(data)
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setImageUrl(data.secure_url);
         setLoading(false);
-    } else {
-      setLoading(false)
+        setError(null);
+      } else {
+        throw new Error('Error uploading image');
+      }
+    } catch (error) {
+      setError("Error uploading image. Please try again.");
+      setLoading(false);
       console.error('Error uploading image:', error);
     }
-   
- 
-  
   };
+
   return (
     <div>
-          <div className='pt-16'>
-        {imageUrl ? <Image src={imageUrl} width={'300px'} height={100} /> :
-          <>No se ha cargado imagen</>}
+      <div className='pt-16'>
         {loading ? <p>cargando....</p> : null}
-        <p></p>
-
+        {error ? <p>{error}</p> : null}
+        {imageUrl ? <Image src={imageUrl} width={300}  crop={"scale"} height={100}  alt='cloudinary' /> : null}
         <input
           type="file"
           accept="image/*"
           onChange={handleUpload}
         />
       </div>
-
-
     </div>
   );
-};
-
+}
