f2d4430ed218339e2b65fec6f3d5dc246e457d2a
Nahuel <nahuelescujuri@gmail.com>
2023-12-26 21:08:47 -0300

    fix unidad y secciones 7

--- components/Class/Table.jsx
+++ components/Class/Table.jsx
@@ -20,8 +20,10 @@ export default function Table({ value, color }){
                         key={index}
                         className={`flex-grow-[1] border-l-2 border-${mainColor} first:border-none max-w-[470px]
                         md:border-none`}>
+
                             <h3 className={`text-white bg-${mainColor} text-center py-3`}>{column?.title}</h3>
-                            <div className="p-3 pb-5 text-violet_dark text-[18px] bg-white
+
+                            <div className="p-4 pb-5 text-violet_dark text-[18px] bg-white h-full
                             md:text-[16px]" dangerouslySetInnerHTML={{ __html: column?.content }}></div>
                         </div>
                     )
