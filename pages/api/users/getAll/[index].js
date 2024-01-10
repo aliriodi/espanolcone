d0777a6f6d3745b3d86ac3e5223fac9491997957
Nahuel <nahuelescujuri@gmail.com>
2024-01-10 00:18:51 -0300

    admin fix 11

--- pages/api/users/getAll/[index].js
+++ pages/api/users/getAll/[index].js
@@ -7,63 +7,63 @@ export default async function GetUsersPage(req, res) {
         method,
     } = req;
 
-    switch (method) {
-        case 'GET':
-            // try {
-            //     console.log('CONNECTING TO MONGO DB');
-            //     await dbConnect();
-            //     console.log('CONNECTED TO MONGO DB');
+    try {
+        console.log('CONNECTING TO MONGO DB');
+        await dbConnect();
+        console.log('CONNECTED TO MONGO DB');
 
-            //     const maxResults = 18;
+        const maxResults = 18;
 
-            //     const allUsers = await Users.find().exec();
+        const allUsers = await Users.find().exec();
 
-            //     const filteredUser = allUsers.slice((maxResults * index) - maxResults, maxResults * index);
+        const filteredUser = allUsers.slice((maxResults * index) - maxResults, maxResults * index);
 
-            //     const  output = filteredUser
+        const  output = filteredUser
 
-            //     // Verifica procedencia de solicitud 
-            //     console.log("/////////////////////////////// ",req.headers.accept == "*/*" ? "Solicitud desde Codigo": "Solicitud desde Navegador"," ///////////////////////////////")
+        // Verifica procedencia de solicitud 
+        console.log("/////////////////////////////// ",req.headers.accept == "*/*" ? "Solicitud desde Codigo": "Solicitud desde Navegador"," ///////////////////////////////")
 
-            //     // if(req.headers.accept == "*/*"){
-            //     //     // Solicitud desde el codigo
-            //     //     res.status(200).json(
-            //     //         {
-            //     //             users: output,
-            //     //             // totalUsers: allUsers.length
-            //     //         }
-            //     //     );
-            //     // }
-            //     // else{
-            //     //     // Solicitud desde el navegador
-            //     //     res.status(200).json({ message: "Acceso Denegado" });
-            //     // }
+        // if(req.headers.accept == "*/*"){
+        //     // Solicitud desde el codigo
+        //     res.status(200).json(
+        //         {
+        //             users: output,
+        //             // totalUsers: allUsers.length
+        //         }
+        //     );
+        // }
+        // else{
+        //     // Solicitud desde el navegador
+        //     res.status(200).json({ message: "Acceso Denegado" });
+        // }
 
-            //     res.status(200).json(
-            //         {
-            //             // message:"Hola Mundo"
-            //             users: output,
-            //             totalUsers: allUsers.length
-            //         }
-            //     );
-
-            // }
-
-            // catch (error) {
-            //     console.error("Error :) ",error);
-            //     res.json({ elError:error });
-            // }
-            res.status(200).json(
-                {
-                    message:"Hola Mundo"
-                    // users: output,
-                    // totalUsers: allUsers.length
-                }
-            );
-
-        
-        default:
-            return res.status(405).json({ message: `Method ${method} not allowed` });
+        res.status(200).json(
+            {
+                // message:"Hola Mundo"
+                users: output,
+                totalUsers: allUsers.length
+            }
+        );
 
     }
+
+    catch (error) {
+        console.error("Error :) ",error);
+        res.json({ elError:error });
+    }
+    // res.status(200).json(
+    //     {
+    //         message:"Hola Mundo"
+    //         // users: output,
+    //         // totalUsers: allUsers.length
+    //     }
+    // );
+    // switch (method) {
+    //     case 'GET':
+
+        
+    //     default:
+    //         return res.status(405).json({ message: `Method ${method} not allowed` });
+
+    // }
 }
