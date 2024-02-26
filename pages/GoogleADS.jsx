eb0b5753e16f395c18ad2bcf1fdf82cf56a59328
aliriodi <aliriodi@gmail.com>
2024-02-26 11:25:24 -0300

    id

--- pages/GoogleADS.jsx
+++ pages/GoogleADS.jsx
@@ -2,7 +2,7 @@ import Script from 'next/script';
 
 export const GTMscript = () => (
   <>
-    <Script>{`window.dataLayer = window.dataLayer || [];`}</Script>
+    <Script id="data-layer">{`window.dataLayer = window.dataLayer || [];`}</Script>
     <Script id="google-tag-manager" strategy="afterInteractive">
       {` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
