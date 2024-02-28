00574d19caed059f4ad772a90aea183b4f37e1c1
aliriodi <aliriodi@gmail.com>
2024-02-28 12:15:14 -0300

    GTM otra vez

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import { appWithTranslation } from 'next-i18next'
 import nextI18NextConfig from '../next-i18next.config';
 import Head from 'next/head';
 import GoogleAnalytics from './GoogleAnalytics'
-import GTMscript from './GoogleGTMscript';
+//import GTMscript from './GoogleGTMscript';
 import LoadScreen from '../components/LoadScreen';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
