f0db5f775228812cbb104d6fd9003d1aa7220d3d
aliriodi <aliriodi@gmail.com>
2024-02-28 11:20:53 -0300

    cargando microleccion nuevo modelo

--- components/UlessonLoad/cargarUlesson.js
+++ components/UlessonLoad/cargarUlesson.js
@@ -8,79 +8,98 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 // Función para introducir una variable
- function introducirVariable(pregunta, callback) {
+function introducirVariable(pregunta, callback) {
   rl.question(pregunta, (respuesta) => {
     callback(respuesta);
   });
 }
 
 // Introducir la primera variable
-introducirVariable('Por favor introduce el nombre del json Microclase, example: U1NA1.json: ',  (namefile) => {
+introducirVariable('Por favor introduce el nombre del json Microclase, example: U1NA1.json: ', (namefile) => {
   // Hacer algo con la primera variable
-  console.log('', );
-  
+  console.log('',);
+
   // Introducir la segunda variable
   introducirVariable('Introduce el nombre del nivel Microclase, example: Nivel A1 :  ', (level) => {
     // Hacer algo con la segunda variable
-    console.log('', );
- 
+    console.log('',);
+
     // Introducir la tercera variable
-  introducirVariable('Introduce el nombre de la unidad Microclase, example: Unidad 1 :  ', (unit) => {
-    // Hacer algo con la tercera variable
-    console.log('', );
-    // Introducir la cuarta variable
-    introducirVariable('Introduce una descripcion Recuerda que debe ser unica Microclase: ',async  (description) => {
+    introducirVariable('Introduce el nombre de la unidad Microclase, example: Unidad 1 :  ', (unit) => {
       // Hacer algo con la tercera variable
       console.log('',);
-      introducirVariable('Introduce tags sepaarados por ",": ',async  (tag) => {
+      // Introducir la cuarta variable
+      introducirVariable('Introduce una descripcion Recuerda que debe ser unica Microclase: ', async (description) => {
         // Hacer algo con la tercera variable
-       const tags = tag.split(',')
-      console.log('Procedo a cargar');
-      
-      await imprimir( namefile,level,unit,description,tags)
-      // Cerrar la interfaz de lectura
-      rl.close();
+        console.log('',);
+        introducirVariable('Introduce tags sepaarados por ",": ', async (tag) => {
+          // Hacer algo con la tercera variable
+          const tags = tag.split(',')
+          introducirVariable('Introduce topics o topico ciencia, teoria",": ', async (topics) => {
+            // Hacer algo con la tercera variable
+
+            console.log('',);
+
+            introducirVariable('Introduce formats o tipo de articulo video,articulo, cancion",": ', async (formats) => {
+              // Hacer algo con la tercera variable
+
+              console.log('',);
+
+              // Cerrar la interfaz de lectura
+
+
+              console.log('Procedo a cargar');
+
+              await imprimir(namefile, level, unit, description, tags, topics, formats)
+              // Cerrar la interfaz de lectura
+              rl.close();
+            });
+          });
+        });
+      });
     });
-  });
-  });
-   
+
   });
 });
 
 
 
-async function imprimir(namefile,level,unit,description,tags)
-{const clase = require("./"+namefile);
+async function imprimir(namefile, level, unit, description, tags,topics,formats) {
+  const clase = require("./" + namefile);
 
   //console.log(clase)
-let nuevaclase=[];
-const numberOfPages = Object.keys(clase).length;
-console.log(numberOfPages)
-console.log(Object.keys(clase))
-//cargo las nuevas clases las primeras 11 paginas
-for(i=1;i<numberOfPages+1;i++){
-//mapeo el objeto de clases por numero de paginas 
-//es un OBJETO
-    nuevaclase.push(clase['page'+i])
+  let nuevaclase = [];
+  const numberOfPages = Object.keys(clase).length;
+  console.log(numberOfPages)
+  console.log(Object.keys(clase))
+  //cargo las nuevas clases las primeras 11 paginas
+  for (i = 1; i < numberOfPages + 1; i++) {
+    //mapeo el objeto de clases por numero de paginas 
+    //es un OBJETO
+    nuevaclase.push(clase['page' + i])
 
+  }
+
+  console.log(nuevaclase.length)
+  await fetch('http://localhost:3000/api/ulessons/add/',
+    {  //redirect: 'follow',
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'  // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({
+        level: level,
+        unit: unit,
+        description: description,
+        tags: tags,
+        topics:topics,
+        formats:formats,
+        sheets: nuevaclase
+      })
+
+      //mando por el body la nueva clase
+    })
 }
-
-console.log(nuevaclase.length)
- await fetch('http://localhost:3000/api/ulessons/add/', 
-             {  //redirect: 'follow',
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                 headers: {
-                            'Content-Type': 'application/json'  // 'Content-Type': 'application/x-www-form-urlencoded',
-                          },
-                 body: JSON.stringify({
-                                       level:level,
-                                       unit:unit,
-                                       description:description,
-                                       tags:tags,
-                                       sheets:nuevaclase})
-                                       
-                                       //mando por el body la nueva clase
-             })}
