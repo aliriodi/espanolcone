d6fe3b0244b1d112fa5081a86f73f61b7b59eaf0
aliriodi <aliriodi@gmail.com>
2024-03-11 17:31:26 -0300

    handleRouteChange

--- pages/_app.js
+++ pages/_app.js
@@ -18,11 +18,12 @@ function App({ Component, pageProps }) {
   //Google Tag Manager GTM
 
   useEffect(() => {
-    const handleRouteChange = (url) => {
+
+     const handleRouteChange = (url) => {
       // Registra un evento de visualización de página en cada cambio de ruta
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K4B69KPW');
     };
-
+    handleRouteChange;
     router.events.on('routeChangeComplete', handleRouteChange);
 
     return () => {
