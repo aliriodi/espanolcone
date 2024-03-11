ea65de512bb76abcb7a9ed7b6d6ea895857d8897
aliriodi <aliriodi@gmail.com>
2024-03-11 17:45:35 -0300

    gtm

---
+++ pages/GoogleGTM.jsx
@@ -0,0 +1,20 @@
+
+'use client'
+
+import Script from "next/script"
+
+
+function GTM  ()  {
+
+      return (
+        <div>
+           <Script
+                strategy="afterInteractive">
+                    {(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K4B69KPW')}
+            </Script>
+            <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K4B69KPW" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
+        </div>
+    )
+}
+
+export default GTM
