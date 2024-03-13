828371acd08f68ad26b1154fd368335b1d7cfb10
aliriodi <aliriodi@gmail.com>
2024-03-13 04:36:47 -0300

    fbq check

--- components/ModalPagoPAYPALLanding.js
+++ components/ModalPagoPAYPALLanding.js
@@ -13,98 +13,106 @@ export default function ModalPagoPAYPALL(props) {
     setIsOpen(false)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     setIsOpen(props.open);
 
-  // fbq('track', 'Purchase', {
-  //   value: props.dates.cost,
-  //   currency: 'usd',
-  //   payment:'Paypal'
-  // }  );
+    // fbq('track', 'Purchase', {
+    //   value: props.dates.cost,
+    //   currency: 'usd',
+    //   payment:'Paypal'
+    // }  );
 
-  },[props.open])
+  }, [props.open])
 
   return (
     <>
 
       {isOpen &&
-      <>
-        <div
-        onClick={closeModal}
-        className='fixed w-screen min-h-screen top-0 left-0 bg-[#000000aa] flex flex-col justify-center items-center z-[999]'>
-
+        <>
           <div
-          onClick={(e)=>e.stopPropagation()}
-          className='bg-white rounded-md p-5
+            onClick={closeModal}
+            className='fixed w-screen min-h-screen top-0 left-0 bg-[#000000aa] flex flex-col justify-center items-center z-[999]'>
+
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className='bg-white rounded-md p-5
           md:w-full md:rounded-none'>
 
-            {/* Logo */}
-            <div className=' flex justify-center flex-col border-b-2 pb-5 items-center relative'>
+              {/* Logo */}
+              <div className=' flex justify-center flex-col border-b-2 pb-5 items-center relative'>
 
-              <Image  src={Logo} className='mb-6' style={{ width: '100px' }} alt="Logo" />
+                <Image src={Logo} className='mb-6' style={{ width: '100px' }} alt="Logo" />
 
-              <p className='text-[18px] text-light font-medium absolute bottom-[-13px] bg-white px-2'>Métodos de pago:</p>
-            </div>
+                <p className='text-[18px] text-light font-medium absolute bottom-[-13px] bg-white px-2'>Métodos de pago:</p>
+              </div>
 
-            {/* Metodo de pago */}
-            <div
-            className='w-[750px] max-h-[70vh]  flex justify-center p-3 mt-7
+              {/* Metodo de pago */}
+              <div
+                className='w-[750px] max-h-[70vh]  flex justify-center p-3 mt-7
             overflow-y-scroll modal-paypal
             md:w-full md:px-0'>
-              
-              <div className='w-full m-auto'>
-                {
-                  <PayPalScriptProvider
-                    style={{
-                      width: "100%",
-                    }}
-                    options={{
+
+                <div className='w-full m-auto'>
+                  {
+                    <PayPalScriptProvider
+                      style={{
+                        width: "100%",
+                      }}
+                      options={{
                         "clientId": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
-                    }}
+                      }}
                     >
                       <PayPalButtons
-                      className=''
-                          style={{
-                              color:"blue",
-                              
-                          }}
-                          createOrder={async()=>{
-                            try{
-                            const res =  await axios({
-                                url:"/api/payment",
-                                method: "POST",
-                                headers:{
-                                  "Content-Type": "application/json"
-                                },
-                                data: props?.dates
-                              })
-                              return res.data.id
-                            } catch (error){
-                              console.log(error)
-                            }
-                          }} 
-                          onCancel={(data) => {
-                            console.log("Canceled order:", data);
-                            props.onPaymentCancel();
-                          }}
-                          onApprove={(data, actions)=>{
-                            console.log("Data ",data)
-                            actions.order.capture().then((response) => {
-                            props.onPaymentSuccess(data,response); 
-                            closeModal();
-                            });
-                          }}
-                      />
-                  </PayPalScriptProvider>
-                  ||
-                  <p>loading...</p>
-                }
-              </div>
-            </div>
+                        className=''
+                        style={{
+                          color: "blue",
 
+                        }}
+                        createOrder={async () => {
+                          fbq('track', 'Purchase', {
+                            value: props.dates.cost,
+                            currency: 'usd',
+                            payment: 'Paypal'
+                          });
+
+
+
+                          try {
+                            const res = await axios({
+                              url: "/api/payment",
+                              method: "POST",
+                              headers: {
+                                "Content-Type": "application/json"
+                              },
+                              data: props?.dates
+                            })
+                            return res.data.id
+                          } catch (error) {
+                            console.log(error)
+                          }
+                        }}
+                        onCancel={(data) => {
+                          console.log("Canceled order:", data);
+                          props.onPaymentCancel();
+                        }}
+                        onApprove={(data, actions) => {
+                          console.log("Data ", data)
+                          actions.order.capture().then((response) => {
+                            props.onPaymentSuccess(data, response);
+                            closeModal();
+                          });
+                        }}
+                      />
+                    </PayPalScriptProvider>
+                    ||
+                    <p>loading...</p>
+                  }
+                </div>
+              </div>
+
+            </div>
           </div>
-        </div>
-      </>
+        </>
       }
     </>
   )
--- components/ModalPagoZelleLanding.js
+++ components/ModalPagoZelleLanding.js
@@ -641,7 +641,12 @@ export default function ModalPagoZELLE(props) {
                     {/* Enviar Pago */}
                     {
                     ImageUrl &&
-                    <button className='btn-success px-5 py-2.5 mt-3 w-full  text-[16px]' id={'PruebaGTM'} onClick={() => { PAYOK(); }}>
+                    <button className='btn-success px-5 py-2.5 mt-3 w-full  text-[16px]' id={'PruebaGTM'} onClick={() => { PAYOK();
+                      fbq('track', 'Purchase', {
+                        value: props?.dates?.cost,
+                        currency: 'usd',
+                        payment: 'Zelle'
+                      }); }}>
                       {
                         loading ?
                         <div
