b749e29a6d8ddadd8d3b896400adaccdd349e9f7
Valen <valenzalazar3@gmail.com>
2024-05-22 17:37:15 -0300

    problems solved

--- components/Calendar-Group/Student/ButtonAddStudentCalendarGroup.jsx
+++ components/Calendar-Group/Student/ButtonAddStudentCalendarGroup.jsx
@@ -1,11 +1,52 @@
-"use client"
-import useStudents from '../../../hooks/useStudents';
+import { useState } from 'react';
 import { parseISO, isSameDay } from 'date-fns';
 
 export const ButtonAddStudentCalendarGroup = ({ personSchedule, renders, selectedDay }) => {
 
 
-    const { useAddStudentsCalendar, isSubmitting } = useStudents();
+    const useAddStudentsCalendar = async (student, classDetails) => {
+
+        const { first_name, last_name, image, email, country } = student;
+        const { students, studentLimit, id, startDatetime, endDatetime, teacherEmail } = classDetails;
+
+        // Verifica si el límite de estudiantes se ha superado
+        if (students.length >= studentLimit) {
+            return alert('El límite de estudiantes anotado se ha superado');
+        }
+
+        // Agrega el estudiante a la clase localmente
+        const updatedStudents = [...students, { first_name, last_name, image, email, country }];
+
+        // Crea la estructura de datos actualizada para la clase
+        const updatedClassDetails = {
+            ...classDetails,
+            students: updatedStudents
+        };
+
+
+
+        try {
+            const response = await fetch('/api/users/update', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: teacherEmail, updates: { calendarGroup: updatedClassDetails } }),
+            });
+
+            const data = await response.json();
+            console.log(data);
+            alert('El estudiante se ha anotado correctamente.');
+        } catch (error) {
+            console.error(error);
+            alert('Hubo un problema al anotar al estudiante.');
+        }
+
+        // Imprime el resultado para verificación
+        console.log(`Estudiantes anotados en la clase ${id} (${startDatetime} - ${endDatetime}):`, updatedStudents);
+        console.log('Cantidad de estudiantes anotados:', updatedStudents.length);
+    };
+
 
 
     if (!personSchedule || !personSchedule.calendarGroup || !renders) {
