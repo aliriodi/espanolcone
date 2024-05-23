38d3f5eb7ff7e8d726bad529b0d88cad2bb763d8
Valen <valenzalazar3@gmail.com>
2024-05-22 22:32:37 -0300

    fix all problems with custom hook

--- components/Calendar-Group/Student/ButtonAddStudentCalendarGroup.jsx
+++ components/Calendar-Group/Student/ButtonAddStudentCalendarGroup.jsx
@@ -1,56 +1,10 @@
-import { useState } from 'react';
+
 import { parseISO, isSameDay } from 'date-fns';
+import useAddStudentsCalendarGroup from '../hooks/useAddStudentCalendarGroup';
 
 export default function ButtonAddStudentCalendarGroup({ personSchedule, renders, selectedDay }) {
 
-
-    async function addStudentsCalendar(student, classDetails) {
-
-        const { first_name, last_name, image, email, country } = student;
-        const { students, studentLimit, id, startDatetime, endDatetime, teacherEmail } = classDetails;
-
-
-
-
-
-        // Verifica si el límite de estudiantes se ha superado
-        if (students.length >= studentLimit) {
-            return alert('El límite de estudiantes anotado se ha superado');
-        }
-
-        // Agrega el estudiante a la clase localmente
-        const updatedStudents = [...students, { first_name, last_name, image, email, country }];
-
-        // Crea la estructura de datos actualizada para la clase
-        const updatedClassDetails = {
-            ...classDetails,
-            students: updatedStudents
-        };
-
-
-
-        try {
-            const response = await fetch('/api/users/update', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email: teacherEmail, updates: { calendarGroup: updatedClassDetails } }),
-            });
-
-            const data = await response.json();
-            console.log(data);
-            alert('El estudiante se ha anotado correctamente.');
-        } catch (error) {
-            console.error(error);
-            alert('Hubo un problema al anotar al estudiante.');
-        }
-
-        // Imprime el resultado para verificación
-        console.log(`Estudiantes anotados en la clase ${id} (${startDatetime} - ${endDatetime}):`, updatedStudents);
-        console.log('Cantidad de estudiantes anotados:', updatedStudents.length);
-    };
-
+    const { handleAddStudentsCalendarGroup, isSubmitting } = useAddStudentsCalendarGroup()
 
 
     if (!personSchedule || !personSchedule.calendarGroup || !renders) {
@@ -65,8 +19,8 @@ export default function ButtonAddStudentCalendarGroup({ personSchedule, renders,
                         type="button"
                         className='bg-success text-center text-white rounded-[5px] py-2 mt-4 mx-1'
                         key={hoursMeet.startDatetime}
-                        onClick={() => addStudentsCalendar(renders.user, { ...hoursMeet, teacherEmail: 'teacher@example.com' })}
-
+                        onClick={() => handleAddStudentsCalendarGroup(renders.user, { ...hoursMeet, teacherEmail: personSchedule.email })}
+                        disabled={isSubmitting}
                     >
                         {hoursMeet.startDatetime.split('T')[1]} - {hoursMeet.endDatetime.split('T')[1]}
                     </button>
