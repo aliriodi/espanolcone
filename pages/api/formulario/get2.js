ac643386e912d8edfb879ece0161be30de4d19b4
aliriodi <aliriodi@gmail.com>
2024-10-01 14:07:41 -0300

    cargando forms para la visual

---
+++ pages/api/formulario/get2.js
@@ -0,0 +1,40 @@
+import dbConnect from '../../../config/mongo'
+import Formulario from '../../../models/Formulario2'
+
+
+
+export default async function getAllForms(req, res) {
+
+  try {
+    
+
+    console.log('CONNECTING TO MONGO DB');
+
+    await dbConnect()
+
+    console.log('CONNECTED TO MONGO DB');
+
+
+
+    console.log('LOOKING FORMS');
+
+    const forms = await Formulario.find().exec();
+
+    console.log('FORMS FOUND');
+
+    // Verifica procedencia de solicitud 
+    if(req.headers.accept == "*/*"){
+      // Solicitud desde el 
+       res.status(200).json({ forms });
+    }
+    else{
+      // Solicitud desde el navegador
+      res.status(200).json({ message: "Acceso Denegado" });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.json({ error })
+  }
+}
+
