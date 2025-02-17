9de626543a26e89593cc0243d00fb95c90447b8f
aliriodi <aliriodi@gmail.com>
2025-02-17 14:44:48 -0300

    mejorando el lexico en el formulario de Membresy

--- pages/inicio/admin/formulario.js
+++ pages/inicio/admin/formulario.js
@@ -1,6 +1,7 @@
 import Form1 from '../../../components/Formularios/ViewForm1';
 import Form2 from '../../../components/Formularios/ViewForm2';
 import Form3 from '../../../components/Formularios/ViewForm3';
+import Membresy2 from '../../../components/Formularios/Membresy2';
 import Menu from '../../../components/Menu';
 import React , { useState, useEffect } from 'react'
 import NavBarAdmin from '../../../components/admin/NavBarAdmin';
@@ -9,23 +10,34 @@ function Formulario() {
     const [Formulario1, setFormulario1] = useState(false);
     const [Formulario2, setFormulario2] = useState(false);
     const [Formulario3, setFormulario3] = useState(false);
+    const [Membresy, setMembresy] = useState(false);
   
     const mostrarFormulario1 = () => {
       setFormulario1(true);
       setFormulario2(false);
       setFormulario3(false);
+      setMembresy(false);
     };
   
     const mostrarFormulario2 = () => {
       setFormulario1(false);
       setFormulario2(true);
       setFormulario3(false);
+      setMembresy(false);
     };
   
     const mostrarFormulario3 = () => {
       setFormulario1(false);
       setFormulario2(false);
       setFormulario3(true);
+      setMembresy(false);
+    };
+
+    const mostrarMembresy = () => {
+      setFormulario1(false);
+      setFormulario2(false);
+      setFormulario3(false);
+      setMembresy(true);
     };
   
     return (
@@ -39,11 +51,13 @@ function Formulario() {
           <button className='px-4 py-2 bg-primary text-white rounded-md' onClick={mostrarFormulario1}>Formulario 1</button>
           <button className='px-4 py-2 bg-primary text-white rounded-md' onClick={mostrarFormulario2}>Formulario 2</button>
           <button className='px-4 py-2 bg-primary text-white rounded-md' onClick={mostrarFormulario3}>Formulario 3</button>
+          <button className='px-4 py-2 bg-primary text-white rounded-md' onClick={mostrarMembresy}>Membresy 1</button>
         </div>
   <div className='pt-8'>
         {Formulario1 && <Form1 />}
         {Formulario2 && <Form2 />}
         {Formulario3 && <Form3 />}
+        {Membresy && <Membresy2 />}
         </div>
       </div>
     );
